114de8007c775f3a0b2001024ae3f5c7d05cd628
kauz.h <kauzlein@gmail.com>
2018-05-26 19:18:37 +0300

    9. Math object

---
+++ math.js
@@ -0,0 +1,19 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.ceil(2.4); // >> rounds to 3
+val = Math.floor(2.8); // >> rounds to 2
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,33,4,1,55,6,3);
+val = Math.max(2,33,4,1,55,6,3);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1); //gives a random decimal between 1 and 20
+
+console.log(val);
