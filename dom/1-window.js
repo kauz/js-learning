ae4d1990e26e50ffc440a4322a9b299193d1d6e7
kauz.h <kauzlein@gmail.com>
2018-06-04 22:16:46 +0300

    19. DOM Window

---
+++ dom/1-window.js
@@ -0,0 +1,27 @@
+// WINDOW METHODS, OBJECTS, PROPERTIES
+
+let val;
+
+val = window.outerHeight;
+val = window.outerWidth;
+val = window.innerWidth;
+
+val = window.scrollY;
+
+// location Object
+
+val = window.location;
+val = window.location.hostname;
+val = window.search;
+
+// Redirect
+// window.location.href = 'http://js-learning.test/'
+// window.location.reload();
+val = window.history;
+// window.history.go(-1)
+val = window.history.length;
+
+// Navigator object
+val = window.navigator;
+
+console.log(val);
