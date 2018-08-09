4729f7dea4befe20887e971d35da94487ec0de94
kauz.h <kauzlein@gmail.com>
2018-08-09 09:44:47 +0300

    77. Weather JS [Part 2]

---
+++ weatherjs/app.js
@@ -0,0 +1,14 @@
+// Init weaher class
+const weather = new Weather('Boston', 'MA');
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+
+function getWeather () {
+	weather.getWeather()
+		.then(results => {
+			console.log(results)
+		})
+		.catch(err => console.log(err)); 
+}
