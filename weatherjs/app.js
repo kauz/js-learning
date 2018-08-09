8f8696a9324bcb4a982fb56dd66e837d3e1ba5d5
kauz.h <kauzlein@gmail.com>
2018-08-09 10:33:09 +0300

    79. Weather JS [Part 4]

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,10 +1,25 @@
-// Init weaher class
-const weather = new Weather('Boston', 'MA');
 // Init UI class
 const ui = new UI();
+// Init Local Storage
+const storage = new Storage();
+// Get stored location data
+let weatherLocation = storage.getLocationData();
+// Init weaher class
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+// Change location evene
+document.querySelector('#w-change-btn').addEventListener('click', (e) => {
+	let city = document.querySelector('#city').value;
+	let state = document.querySelector('#state').value;
+	// Change location
+	weather.changeLocation(city, state);
+	// Set location in LS
+	storage.setLocationData(city, state);
+	getWeather();
+	$('#locModal').modal('hide');
+});
 
 
 function getWeather () {
