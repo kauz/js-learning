df48e83a9f747aa243e4eecbf631f121446a4c85
kauz.h <kauzlein@gmail.com>
2018-08-13 09:23:21 +0300

    82. RegExp [Part 2]

--- regExp/app.js
+++ regExp/app.js
@@ -1,3 +1,6 @@
+/*
+@[Part 1]
+
 let re;
 re = /hello/;
 re = /hello/g; // g- global search
@@ -26,3 +29,39 @@ console.log(result);
 
 result = str.replace(re, 'STAY FUCKIN RIGHT');
 console.log(result.toUpperCase());
+*/
+
+
+
+// [Part 2]
+
+let re;
+// Literal Characters
+re = /hello/;
+
+// Metacharacter Symblos
+re = /^h/i; 		// Must start with
+re = /d$/i; 		// Must ends with
+re = /^hello$/i; 	// Must begin & end with
+re = /.ello/; 		// Matches any single character
+re = /h*o/i;		// Matches any character 0 or more times
+re = /gre?a?y/i;	// Optinal character (could be gray, grey or gry)
+re = /gre?a?y\?/i;			// Escape characters 
+
+
+// String to match
+let str = 'Hello Wolrd, mr. Grey?';
+
+// Log results
+let result = re.exec(str);
+console.log(result);
+
+function reTest (re, str) {
+	if (re.test(str)) {
+		console.log(`${str} matches ${re.source}`);
+	} else {
+		console.log(`${str} does NOT match ${re.source}`);
+	}
+}
+
+reTest(re, str);
